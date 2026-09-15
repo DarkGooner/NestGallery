988251811388fc78fc5f55365a04d813a7f3d2c5
DarkGooner <shadyenterprises.corp@gmail.com>
2026-09-15 22:04:45 +0000

    fix max width import

--- app/src/main/java/com/nestgallery/viewer/ui/ImageViewerScreen.kt
+++ app/src/main/java/com/nestgallery/viewer/ui/ImageViewerScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.pager.HorizontalPager
